5144df3b912bcfe9eda15d5dc3ae7a4acb0e1c93
GuteL <leon.gute4@gmail.com>
2025-04-30 09:18:05 +0000

    u

--- test.py
+++ test.py
@@ -1,5 +1,37 @@
 def main(): 
-    print("Hello, World!")
+    global zahl1	
+    zahl1 = input("Gib eine Zahl ein: ")	
+    global zahl2
+    zahl2 = input("Gib eine zweite Zahl ein: ")
+    global rechenart
+
+    # Überprüfen, ob die Rechenart korrekt ist
+    while True:
+        rechenart = input("Gib die Rechenart ein: ")
+        if rechenart in ["+", "-", "*", "/"]: 
+            break  
+        else:
+            print("Die Rechenart ist nicht korrekt. Bitte versuche es erneut.")
+    
+    print(f"{zahl1} {rechenart} {zahl2}")
+    rechne()
+    
+    
+def rechne():
+    global zahl1
+    global zahl2
+    global rechenart
+    if rechenart == "+":    
+        print(f"{zahl1} + {zahl2} = {int(zahl1) + int(zahl2)}")
+    elif rechenart == "-":  
+        print(f"{zahl1} - {zahl2} = {int(zahl1) - int(zahl2)}")
+    elif rechenart == "*":
+        print(f"{zahl1} * {zahl2} = {int(zahl1) * int(zahl2)}")
+    elif rechenart == "/":
+        if int(zahl2) == 0:  
+            print("Durch 0 teilen ist nicht erlaubt")
+        else:
+            print(f"{zahl1} / {zahl2} = {int(zahl1) / int(zahl2)}")
+
 
 main()
-
