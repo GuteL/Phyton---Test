b26fe63b147d207b650b581e696a5b0b1b604faa
GuteL <leon.gute4@gmail.com>
2025-07-09 09:35:53 +0000

    u

--- tok.py
+++ tok.py
@@ -1,70 +1,84 @@
 import datetime
 import requests
-# import json
-
-# ----------------------------- Globale - Variablen --------------------------------------
-username = "example_user"
-password = "example_password"
-# ----------------------------- Klassen --------------------------------------------------
-
-class Tokenmanager:
-    global username, password
-    
-    def __init__(self, url, headers, payload):
-        self.url = url
-        self.headers = headers
-        self.payload = payload
-        self.token = None
-        self.expirytime = datetime.datetime.now()
-        self.status = None
+from queue import Queue
+from threading import Thread  
+# import json 
  
-    def get_token(self):
-        response = requests.post(self.url, data=self.payload)
-        data = response.json()
-        self.token = data.get("access_token")
-        self.expirytime = datetime.datetime.now() + datetime.timedelta(seconds=890)
-        self.status = response.raise_for_status()
-        print("Neuer Token wurde generiert")
-        return self.token
-
-    def test_token(self):
-        if self.expirytime <= datetime.datetime.now():
-            print("Token ist abgelaufen")
-            self.token = self.get_token()
-            return self.token
-        else:
-            print("Token ist noch gültig")
-
-
-# -------------------------- Main Fluss -----------------------------------------
-manager = Tokenmanager(url="example.com", headers={"":""}, payload={"username": username, "password": password})
-print(manager.get_token())
-
-# -------------------------------------------------------------------------------
-# -------------------------- ohne Klassen ---------------------------------------
 
 
 # # ------------------------ Globale - Variablen --------------------------------
-# token = None
-# expirytime = None
+TOKEN = None
+EXPIRYTIME = None
+NUMBER_OF_CONSUMERS = 3
 # -------------------------- Funktionen -----------------------------------------
 
+def get_headers(): 
+    global TOKEN, EXPIRYTIME
+    if EXPIRYTIME is None or EXPIRYTIME <= datetime.datetime.now():
+        print("Der Token ist abgelaufen")
+        response = requests.post("example.com", data={"":""})
+        data = response.json()
+        TOKEN = data.get("access_token")
+        EXPIRYTIME = datetime.datetime.now() + datetime.timedelta(seconds=890)
+        print("Neuer Token wurde generiert")
+        header = {
+            "Authorization": f"Bearer {TOKEN}",
+            "Accept": "application/pdf",
+        }
+        return header
+    else:
+        expirationTime = EXPIRYTIME - datetime.datetime.now()
+        print(f"Der Token ist noch gültig für: {expirationTime}")
+        header = {
+            "Authorization": f"Bearer {TOKEN}",
+            "Accept": "application/pdf",
+        }
+        return header
+
+def producer(queue, items):
+    print("producer: Daten werden generiert...")
+    for item in items:
+        queue.put(item)
+    queue.put(None)
+    print (f"producer: {queue.qsize()} Daten in der Queue")
+    print(f"Anzahl der Consumer: {NUMBER_OF_CONSUMERS}")
+    
+    
+
 # def get_token():
-#     global token, expirytime
+#     global TOKEN, expirytime
 #     response = requests.post("example.com", data={"":""})
 #     data = response.json()
-#     token = data.get("access_token")
+#     TOKEN = data.get("access_token")
 #     expirytime = datetime.datetime.now() + datetime.timedelta(seconds=890)
 #     print("Neuer Token wurde generiert")
-#     return token, expirytime
+#     return TOKEN, expirytime
 
 # def check_token():
-#     global token, expirytime
+#     global TOKEN, expirytime
 #     if expirytime <= datetime.datetime.now():
 #         print("Token ist abgelaufen")
-#         token, expirytime = get_token()
-#         return token, expirytime
+#         TOKEN, expirytime = get_token()
+#         return TOKEN, expirytime
 #     else:
 #         print("Token ist noch gültig")
-#         return token, expirytime
+#         return TOKEN, expirytime
     
+    
+    
+# -------------------------- Funktionen - Ablauf -----------------------------------------
+    
+
+# get_token()
+# get_registrationprotocols()
+#  check_token()
+# get_pdf_Name()
+#  get_buisnesspremisesID()
+    # check_token()
+#  get_time()
+
+
+ 
+# get_pdf()
+#  check_token()
+# send_pdf() v upload_pdf()
